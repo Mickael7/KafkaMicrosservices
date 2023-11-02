5a763b32c8d0c8a5a14dc27e2d8bff12acee322c
pedrocez <pedrocecarvalho@gmail.com>
2023-11-02 14:55:10 -0300

    Primeiro commit

---
+++ src/main/java/com/portal/api/client/CarPostStoreClient.java
@@ -0,0 +1,43 @@
+package com.portal.api.client;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.portal.api.dto.CarPostDTO;
+import com.portal.api.dto.OwnerPostDTO;
+
+@Component
+public class CarPostStoreClient {
+
+	private final String USER_STORE_SERVICE_URI = "http://localhost:8080/user";
+
+	private final String POSTS_STORE_SERVICE_URI = "http://localhost:8080/sales";
+
+	@Autowired
+	RestTemplate restTemplate;
+
+
+	public List<CarPostDTO> carForSaleClient(){
+		  ResponseEntity<CarPostDTO[]> responseEntity = restTemplate.getForEntity(POSTS_STORE_SERVICE_URI+"/cars",CarPostDTO[].class);  
+		  return Arrays.asList(Objects.requireNonNull(responseEntity.getBody()));
+	  }
+
+	public void ownerPostClient(OwnerPostDTO newUser) {
+		restTemplate.postForEntity(USER_STORE_SERVICE_URI, newUser, OwnerPostDTO.class);
+	}
+
+	public void changeForSaleClient(CarPostDTO carPostDTO, String id) {
+		restTemplate.put(POSTS_STORE_SERVICE_URI + "/car/" + id, carPostDTO, CarPostDTO.class);
+	}
+
+	public void deleteForSaleClient(String id) {
+		restTemplate.delete(POSTS_STORE_SERVICE_URI+"/car/"+id);
+	}
+
+}
---
+++ src/main/java/com/portal/api/service/CarPostStoreServiceimpl.java
@@ -0,0 +1,35 @@
+package com.portal.api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.portal.api.client.CarPostStoreClient;
+import com.portal.api.dto.CarPostDTO;
+
+@Service
+public class CarPostStoreServiceimpl implements CarPostStoreService{
+
+	@Autowired
+	private CarPostStoreClient carPostStoreClient;
+	 
+	@Override
+	public List<CarPostDTO> getCarForSales() {
+		return carPostStoreClient.carForSaleClient();
+	}
+
+	@Override
+	public void changeCarForSale(CarPostDTO carPost, String id) {
+		carPostStoreClient.changeForSaleClient(carPost, id);
+	}
+
+	@Override
+	public void removeCarForSale(String id) {
+		carPostStoreClient.deleteForSaleClient(id);
+	}
+
+	
+	
+	
+}
